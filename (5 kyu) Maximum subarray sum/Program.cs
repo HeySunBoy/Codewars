d1b006d87a62fdb3b983c660067d8db8d3004d5f
GaryChen <tp841025@gmail.com>
2020-08-30 14:16:24 +0800

    1. 2020/08/30 Practice Count: 2

---
+++ (5 kyu) Maximum subarray sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _5_kyu__Maximum_subarray_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(",", MaxSequence(new int[] { })));
+        }
+
+        public static int MaxSequence(int[] arr)
+        {
+            if (arr.Count(n => n >= 0) == 0)
+                return 0;
+           
+            int max = int.MinValue;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    sum += arr[j];
+                    max = sum > max ? sum : max;
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ (6 kyu) Decode the Morse code/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+
+namespace _6_kyu__Decode_the_Morse_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Decode(".... . -.--   .--- ..- -.. ."));
+        }
+
+        public static string Decode(string morseCode)
+        {
+            morseCode = morseCode.Trim().Replace("   ", "  ");
+
+            string[] s = morseCode.Split(new char[] { ' ', '\r', '\n' });
+
+            return string.Concat(s.Select(ss => TransMorseCodeToWord(ss)).ToArray());
+        }
+
+        static string TransMorseCodeToWord(string strMorseCode)
+        {
+            switch (strMorseCode)
+            {
+                case ".-":
+                    return "A";
+                case "-...":
+                    return "B";
+                case "-.-.":
+                    return "C";
+                case "-..":
+                    return "D";
+                case ".":
+                    return "E";
+                case "..-.":
+                    return "F";
+                case "--.":
+                    return "G";
+                case "....":
+                    return "H";
+                case "..":
+                    return "I";
+                case ".---":
+                    return "J";
+                case "-.-":
+                    return "K";
+                case ".-..":
+                    return "L";
+                case "--":
+                    return "M";
+                case "-.":
+                    return "N";
+                case "---":
+                    return "O";
+                case ".--.":
+                    return "P";
+                case "--.-":
+                    return "Q";
+                case ".-.":
+                    return "R";
+                case "...":
+                    return "S";
+                case "-":
+                    return "T";
+                case "..-":
+                    return "U";
+                case "...-":
+                    return "V";
+                case ".--":
+                    return "W";
+                case "-..-":
+                    return "X";
+                case "-.--":
+                    return "Y";
+                case "--..":
+                    return "Z";
+                case ".----":
+                    return "1";
+                case "..---":
+                    return "2";
+                case "...--":
+                    return "3";
+                case "....-":
+                    return "4";
+                case ".....":
+                    return "5";
+                case "-....":
+                    return "6";
+                case "--...":
+                    return "7";
+                case "---..":
+                    return "8";
+                case "----.":
+                    return "9";
+                case "-----":
+                    return "10";
+                case "...---...":
+                    return "SOS";
+                case "-.-.--":
+                    return "!";
+                case ".-.-.-":
+                    return ".";
+                default:
+                    return " ";
+            }
+        }
+    }
+}
